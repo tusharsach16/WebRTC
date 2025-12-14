7782278ecbebab1c64812ea28677ab2b4c11a15c
Tushar <tusharschdeva16@gmail.com>
2025-12-15 02:19:15 +0530

    Add button for leave call, camera and mic

--- frontend/src/components/VideoCall.tsx
+++ frontend/src/components/VideoCall.tsx
@@ -1,9 +1,10 @@
 import React, {useRef} from "react";
 import {useWebRTC} from "../hooks/useWebRTC";
 
+
 export const VideoCall: React.FC = () => {
     const roomInputRef = useRef<HTMLInputElement| null>(null);
-    const {localVideoRef, remoteVideoRef, status, joinRoom} = useWebRTC();
+    const {localVideoRef, remoteVideoRef, status, joinRoom, toggleMic, toggleCamera, leaveCall} = useWebRTC();
 
     const handleJoin = () => {
         const roomId = roomInputRef.current?.value.trim();
@@ -24,6 +25,11 @@ export const VideoCall: React.FC = () => {
               />
               <button onClick={handleJoin}>Join</button>
               <span style={{ marginLeft: "1rem" }}>Status: {status}</span>
+            </div>
+            <div style={{ marginBottom: "1rem", display: "flex", gap: "0.5rem" }}>
+              <button onClick={toggleMic}>Mute / Unmute</button>
+              <button onClick={toggleCamera}>Camera On / Off</button>
+              <button onClick={leaveCall}>Leave</button>
             </div>
 
             <div style={{ display: "flex", gap: "1rem" }}>
